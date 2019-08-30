48c1e455d71df0ff4d233481dcedacee7c2f0f7e
Damian Lopez <damianlopez@Damians-MacBook-Pro.local>
2019-08-30 10:02:54 -0600

    Added the table view

---
+++ Meal.swift
@@ -0,0 +1,21 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Damian Lopez on 8/29/19.
+//  Copyright © 2019 Damian Lopez. All rights reserved.
+//
+
+import UIKit
+
+class Meal: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
